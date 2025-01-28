29d599ebbf1a15447b2fdecc98007235835574b5
anmanosca@gmail.com <Scarpa, Andrea>
2025-01-28 19:08:41 +0100

    feat: create wrap fn pipe

--- apps/angular/9-wrap-function-pipe/src/app/app.component.ts
+++ apps/angular/9-wrap-function-pipe/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { WrapFnPipe } from './wrap-function.pipe';
 
 @Component({
-  imports: [NgFor],
+  imports: [WrapFnPipe],
   selector: 'app-root',
   template: `
-    <div *ngFor="let person of persons; let index = index; let isFirst = first">
-      {{ showName(person.name, index) }}
-      {{ isAllowed(person.age, isFirst) }}
-    </div>
+    @for (person of persons; track person) {
+      <div>
+        {{ showName | wrapFn: person.name : $index }}
+        {{ isAllowed | wrapFn: person.age : $first }}
+      </div>
+    }
   `,
 })
 export class AppComponent {
